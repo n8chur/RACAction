da6f2c48184426e92ba1bb425264343a31e649df
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-05-21 12:18:15 -0700

    Set up project with iOS target

---
+++ RACAction/RACAction.h
@@ -0,0 +1,19 @@
+//
+//  RACAction.h
+//  RACAction
+//
+//  Created by Justin Spahr-Summers on 2015-05-21.
+//  Copyright (c) 2015 Automatic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RACAction.
+FOUNDATION_EXPORT double RACActionVersionNumber;
+
+//! Project version string for RACAction.
+FOUNDATION_EXPORT const unsigned char RACActionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RACAction/PublicHeader.h>
+
+
